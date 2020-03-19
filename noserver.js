29b9e7a4e5d41426ab3c7d5dedaca449f5c6101e
Oswaldo Gómez <gosocosdav@gmail.com>
2020-03-19 01:01:02 -0300

    Update and rename noserver.js to server.js

--- noserver.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function(req,res){
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('/');
-});
-console.log('Server started on port 80; press Ctrl-C to terminate...!');
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express-handlebars');
+var app = express();
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running);
+});
