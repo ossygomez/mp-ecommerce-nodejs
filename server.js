7e5ca3eede81cee2d48c890726c640e152273f71
Oswaldo Gómez <gosocosdav@gmail.com>
2020-03-19 00:32:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req,res){
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('/');
-}).listen(80);
+});
 console.log('Server started on port 80; press Ctrl-C to terminate...!');
